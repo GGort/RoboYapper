7e84b8ce7c7452b505bd7c89a1eb94c926194ec0
Gerjan Gort <gj.g@live.nl>
2024-11-20 16:41:25 +0100

    absolute minimum (everything else will come as needed)

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,30 @@
 <script>
 	import '../app.css';
 	let { children } = $props();
+  import { page } from '$app/stores';
+
+  import { Navbar, NavBrand, NavUl, NavLi , NavHamburger, DarkMode, Footer} from "flowbite-svelte";
+
+  let activeUrl = $derived($page.url.pathname);
 </script>
 
+<div class="min-h-screen">
+
+<Navbar>
+  <NavBrand>Roboyapper</NavBrand>
+
+  <div class="flex flex-row">
+    <NavUl {activeUrl}>
+      <NavLi href="/">Home</NavLi>
+    </NavUl>
+    <NavHamburger/>
+
+    <DarkMode />
+  </div>
+</Navbar>
+
 {@render children()}
+</div>
+<Footer>
+
+</Footer>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,23 +1,6 @@
 <script lang="ts">
-  import { Alert } from "flowbite-svelte";
-  import type { PageData } from "../../.svelte-kit/types/src/routes/$types";
-
-  let { data }: { data: PageData } = $props();
-
-  console.log(data);
 </script>
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<div class="p-8">
-  <Alert>
-    <span class="font-medium">Info alert!</span>
-    Change a few things up and try submitting again.
-  </Alert>
+<div class="p-5">
+  <h1 class="H1">HomePage</h1>
 </div>
-
-<ul>
-  {#each data.roles ?? [] as role}
-    <li>{role.id}: {role.name}</li>
-  {/each}
-</ul>
